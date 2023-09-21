4e80012bb77d2925be37a63cf8cbedbfbeb94170
pasaismihan <pasa_13142@hotmail.com>
2023-09-21 12:10:38 +0300

    vercel.json eklendi

--- .next/server/font-loader-manifest.js
+++ .next/server/font-loader-manifest.js
@@ -1,11 +1,1 @@
-self.__FONT_LOADER_MANIFEST={
-  "pages": {
-    "/_app": [
-      "static/media/beb742c171df2176-s.p.woff2",
-      "static/media/122c360d7fe6d395-s.p.woff2"
-    ]
-  },
-  "app": {},
-  "appUsingSizeAdjust": false,
-  "pagesUsingSizeAdjust": true
-}
+self.__FONT_LOADER_MANIFEST={pages:{"/_app":["static/media/beb742c171df2176-s.p.woff2","static/media/122c360d7fe6d395-s.p.woff2"]},app:{},appUsingSizeAdjust:!1,pagesUsingSizeAdjust:!0};
